217c4da847c5e903e181b861f6113d780158ac85
Amine <amichachi@alum.us.es>
2021-06-22 00:51:53 +0200

    Restructuración del Proyecto

--- frontend/src/services/Comunidad/comunity.service.js
+++
@@ -1,9 +0,0 @@
-import http from "../../http-common";
-import authHeader from '../Authentication/auth-header'
-
-export default class ComunityDataService {
-
-    getAllComunitys() {
-        return http.get(`/comunidades/`,{headers: authHeader()});
-    }
-}
