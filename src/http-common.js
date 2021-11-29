a535d2c51e1a798c1ce9666be3b32a768f7597ce
Amine <amichachi@alum.us.es>
2021-11-29 20:28:47 +0100

    Cambio de URL para el despliegue

--- src/http-common.js
+++ src/http-common.js
@@ -1,8 +1,8 @@
   import axios from 'axios'
 
 export default axios.create({
-  //baseURL: 'https://blackmambaback.oa.r.appspot.com/api/',
-  baseURL: 'http://localhost:3000/api/',
+  baseURL: 'https://superb-garden-333619.ew.r.appspot.com//api/',
+  //baseURL: 'http://localhost:3000/api/',
   headers: {
     'Content-type': 'application/json',
   },
